785a9b74d52bee0de47b4ef688baedb5389ceb3c
Yanfgomes <yangomes2000@gmail.com>
2025-09-08 22:29:31 -0300

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -38,7 +38,7 @@ ALLOWED_HOSTS = [
 # Application definition
 
 INSTALLED_APPS = [
-    "mysite.polls",
+    "mysite.polls.apps.PollsConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
