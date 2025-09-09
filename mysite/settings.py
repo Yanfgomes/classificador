253f1fc927c71e0fe198f43b42d95f5e76569ba9
Yanfgomes <yangomes2000@gmail.com>
2025-09-08 22:16:42 -0300

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
+        default=os.environ["DB_CONNECTION_STRING"],
         conn_max_age=600
     )
 }
